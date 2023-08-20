f566aef57159b63cb4ec5a4a5b59353063aa4205
Sardor <sardorsoftwaredeveloper@gmail.com>
2023-08-20 12:17:10 +0200

    Postgres SQL changes

--- aahelfer/settings.py
+++ aahelfer/settings.py
@@ -78,10 +78,21 @@ WSGI_APPLICATION = 'aahelfer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'aahelferDB',
+        'USER': 'sardor_admin',
+        'PASSWORD': 'sardor1001@',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 
 # Password validation
